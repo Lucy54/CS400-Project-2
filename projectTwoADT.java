30db2db72a7a3b18c6dd9a8a6bdfeef6e5b0dd68
Lucy54 <skuo8@wisc.edu>
2021-08-18 11:32:02 -0500

    Create projectTwoADT.java

---
+++ projectTwoADT.java
@@ -0,0 +1,14 @@
+// --== CS400 File Header Information ==--
+// Name: <Shin-Tsz Lucy Kuo>
+// Email: <skuo8@wisc.edu>
+// Team: <your team name: GD>
+// TA: <Dan Kiel>
+// Lecturer: <Gary Dahl>
+// Notes to Grader: <optional extra notes>
+public interface projectTwoADT {
+  
+  public boolean addPlayer(Player player);
+  public Player getPlayer(int jerseyNumber);
+  public boolean playerExists(int jerseyNumber);
+  
+}
